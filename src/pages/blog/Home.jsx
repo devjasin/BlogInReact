778c915e44ec01c3a967ae5ff31bb758dd212105
devjasin <developer.jasin@gmail.com>
2025-04-23 19:56:11 +0545

    error aaisake paxi naya garako blog fetch samma xat

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import Card from "./components/card/Card";
+import axios from "axios";
+import { baseUrl } from "../../config";
 
 const Home = () => {
+  const [nblogs, setBlogs] = useState([]);
+  const fetchBlog = async () => {
+    const response = await axios.get(`${baseUrl}/blog`);
+    if (response.status === 200) {
+      setBlogs(response.data.data);
+    }
+  };
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
   return (
     <Layout>
       <div className="flex flex-wrap justify-center space-x-2 space-y-2 ">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {nblogs.length > 0 &&
+          nblogs.map((blog) => {
+            return <Card blog={blog} />;
+          })}
       </div>
     </Layout>
   );
